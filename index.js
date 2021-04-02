db4a75ef686e7b0104e8b90198a2d0e1db6e3323
Valerie Russell <vruss14@gmail.com>
2021-04-02 13:54:12 -0400

    added additional questions and switch statement

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-let employeeOptions = ["Engineer", "Intern"];
+let employeeOptions = ["Engineer", "Intern", "Finished building my team!"];
 
-inquirer
-    .prompt([
+const profileQuestions = {
+    Standard: [
         {
             type: 'input',
             message: 'What is the name of the team manager?',
@@ -35,5 +35,80 @@ inquirer
             name: 'employee',
             choices: employeeOptions
         },
+    ],
 
-    ])
+    Engineer: [
+        {
+            type: 'input',
+            message: "What is the engineer's name?",
+            name: "engineername",
+        },
+
+        {
+            type: 'input',
+            message: "What is the engineer's employee ID?",
+            name: "engineerID",
+        },
+
+        {
+            type: 'input',
+            message: "What is the engineer's email address?",
+            name: "engineeremail",
+        },
+
+        {
+            type: 'input',
+            message: "What is the engineer's GitHub username?",
+            name: "engineergithub",
+        },
+    ],
+
+    Intern: [
+
+        {
+            type: 'input',
+            message: "What is the intern's name?",
+            name: "internname",
+        },
+
+        {
+            type: 'input',
+            message: "What is the intern's employee ID?",
+            name: "internID",
+        },
+
+        {
+            type: 'input',
+            message: "What is the intern's email address?",
+            name: "internemail",
+        },
+
+        {
+            type: 'input',
+            message: "What is the intern's school?",
+            name: "internschool",
+        },
+    ]
+}
+
+const askProfileQuestions = async () => {
+    const { employee } = await inquirer.prompt(profileQuestions.Standard)
+
+    switch (employee.toLowerCase()) {
+        case "engineer":
+            await inquirer.prompt(profileQuestions.Engineer);
+            inquirer.prompt(profileQuestions.Standard[4]);
+            break;
+
+        case "intern":
+            await inquirer.prompt(profileQuestions.Intern);
+            inquirer.prompt(profileQuestions.Standard[4]);
+            inquirer.prompt(profileQuestions.Intern);
+            break;
+
+        case "Finished building my team!":
+            break;
+    }
+}
+
+askProfileQuestions();
